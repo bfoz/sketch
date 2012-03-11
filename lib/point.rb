0903046b64b631de10d7e907b2292c3a79e64a6a
Brandon Fosdick <bfoz@bfoz.net>
2012-03-10 16:49:24 -0800

    Sketch::Point subclasses Geometry::Point

--- lib/point.rb
+++ lib/point.rb
@@ -1,34 +1,7 @@
+require 'geometry'
 require 'matrix'
 
 class Sketch
-    class Point
-	attr_reader :x, :y
-	def initialize(*args)
-	    args[0] = args[0].to_a if args[0].is_a?(Vector)
-	    args = args[0] if args[0].is_a?(Array)
-	    @elements = args
-	    @x = args[0] if args[0]
-	    @y = args[1] if args[1]
-	end
-	
-	def Point.[](*array)
-	    new *array
-	end
-
-	def [](i)
-	    @elements[i]
-	end
-	def inspect
-	    'Point(' + @elements.inspect + ')'
-	end
-	def size
-	    @elements.size
-	end
-	def to_a
-	    @elements.dup
-	end
-	def to_s
-	    'Point(' + @elements.join(',') + ')'
-	end
+    class Point < Geometry::Point
     end
 end
--- test/test_point.rb
+++ test/test_point.rb
@@ -4,10 +4,8 @@ require_relative '../lib/point'
 
 class PointTest < Test::Unit::TestCase
     must "create a Point object" do
-	point = Sketch::Point.new 2, 1
-	assert_equal(2, point.size)
-	assert_equal(2, point.x)
-	assert_equal(1, point.y)
+	point = Sketch::Point[2, 1]
+	assert_kind_of(Sketch::Point, point)
     end
     must "create a Point object using list syntax" do
 	point = Sketch::Point[2,1]
@@ -16,13 +14,13 @@ class PointTest < Test::Unit::TestCase
 	assert_equal(1, point.y)
     end
     must "create a Point object from an array" do
-	point = Sketch::Point.new [3,4]
+	point = Sketch::Point[3,4]
 	assert_equal(2, point.size)
 	assert_equal(3, point.x)
 	assert_equal(4, point.y)
     end
     must "create a Point object from a Vector" do
-	point = Sketch::Point.new Vector[3,4]
+	point = Sketch::Point[Vector[3,4]]
 	assert_equal(2, point.size)
 	assert_equal(3, point.x)
 	assert_equal(4, point.y)
@@ -34,7 +32,7 @@ class PointTest < Test::Unit::TestCase
 	assert_equal(4, point.y)
     end
     must "allow indexed element access" do
-	point = Sketch::Point.new [5,6]
+	point = Sketch::Point[5,6]
 	assert_equal(2, point.size)
 	assert_equal(5, point[0])
 	assert_equal(6, point[1])
