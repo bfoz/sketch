c423975d6ec9dc3b15f67aaaa3e6aec75535e2ce
Brandon Fosdick <bfoz@bfoz.net>
2014-01-20 21:37:12 -0800

    Removed the edge command from PolygonBuilder
    It wasn't ever used and it doesn't make sense. If there's ever a need for it, it can be added to the appropriate DSL module.

--- lib/sketch/polygon.rb
+++ lib/sketch/polygon.rb
@@ -4,21 +4,10 @@ class Sketch
     Polygon = Geometry::Polygon
 
     class PolygonBuilder < PolylineBuilder
-	Edge = Geometry::Edge
-
 	# @return [Polygon] the {Polygon} resulting from evaluating the given block
 	def evaluate(&block)
 	    super
 	    Polygon.new *@elements
 	end
-
-	# @group Primitive creation
-
-	# Create and append a new Edge object
-	def edge(*args)
-	    @elements.push Edge.new(*args)
-	end
-
-	# @endgroup
     end
 end
